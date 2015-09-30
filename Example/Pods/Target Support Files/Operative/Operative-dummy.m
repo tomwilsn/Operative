8339ff9400185199f3584d9218dab1ecc2cd71c1
Colin McArdell <colin@colinmcardell.com>
2015-09-30 16:06:00 -0400

    Updates `.gitignore` in example project and removes `/Pods`, `Podfile.lock`, and workspace.
    Let’s leave the installation of dependencies of the example project up
    to the consumer.

--- Example/Pods/Target Support Files/Operative/Operative-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Operative : NSObject
-@end
-@implementation PodsDummy_Operative
-@end
--- Example/Pods/Target Support Files/Pods-Operative_Tests/Pods-Operative_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Operative_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Operative_Tests
-@end
